633e8ec3740d51a5a0b2fd9a0e9f2359d66398cf
Yerkebulan <yerkebulan014@gmail.com>
2023-01-16 14:54:36 +0100

    Update project_work_cmd.py

--- project_work_cmd.py
+++ project_work_cmd.py
@@ -12,15 +12,51 @@ while True:
     elif command == "cd ..":
         list_path = path.split("/")
         path = ""
-        if path != "C:/":
+        if path != "/":
             list_path.remove(list_path[-2])
             list_path.remove(list_path[-1])
             for i in list_path:
                 path += f"{i}/"
     elif command.startswith("cd"):
-        a = command.split()
-        if not a[1] in list_ls:
+        xxx = command.split()
+        if not xxx[1] in list_ls:
             print('No such directory')
         else:
-            path += f"{a[1]}/"
-    
+            path += f"{xxx[1]}/"
+
+    # Creating a 'folder' by mkdir and 'file' by mkfile
+
+    elif command.startswith("mkdir"):
+        xxx = command.split()
+        os.mkdir(path + xxx[1])
+
+    elif command.startswith("mkfile"):
+        xxx = command.split()
+        f = open(path + xxx[1], "x")
+
+
+    # Renaming a directory or file
+
+    elif command.startswith("rename"):
+        xxx = command.split()
+        os.rename(path + xxx[1], path + xxx[2])
+
+
+    # Removing a directory or file
+
+    elif command.startswith("remove"):
+        xxx = command.split()
+        try:
+            os.rmdir(path+xxx[1])
+        except:
+            os.remove(path+xxx[1])
+
+    elif command.startswith("open"):
+        xxx = command.split()
+        f = open(path+xxx[1])
+        for line in f:
+            print(line)
+        f.close()
+
+    elif command == "quit":
+        sys.exit(0)
